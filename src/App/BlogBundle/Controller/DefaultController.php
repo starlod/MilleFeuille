cfae64b728671d054b14b65eb8414dc80cbc01b7
yuki <starlod.ggl@gmail.com>
2016-03-29 20:46:24 +0900

    add AppBlogBundle
    php app/console generate:bundle --namespace=App/BlogBundle --format=annotation

---
+++ src/App/BlogBundle/Controller/DefaultController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\BlogBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+
+class DefaultController extends Controller
+{
+    /**
+     * @Route("/")
+     */
+    public function indexAction()
+    {
+        return $this->render('AppBlogBundle:Default:index.html.twig');
+    }
+}
