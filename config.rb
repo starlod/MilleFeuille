69c020d38d0cddc751a04bce46f2396158cde6be
yuki <starlod.ggl@gmail.com>
2016-03-31 11:39:40 +0900

    fix コード整備

--- config.rb
+++ config.rb
@@ -3,17 +3,17 @@ require 'compass/import-once/activate'
 
 # プロジェクトのディレクトリをここにセットする
 http_path       = '/'
-css_dir         = 'src/App/BlogBundle/Resources/public/css'    #CSSを書き出すディレクトリ
-sass_dir        = 'src/App/BlogBundle/Resources/public/scss'   #SASSファイルがあるディレクトリ
-images_dir      = 'src/App/BlogBundle/Resources/public/images' #画像用のディレクトリ
-javascripts_dir = 'src/App/BlogBundle/Resources/public/js'     #JavaScriptファイル用のディレクトリ
-cache_dir       = '../.sass-cache'                             #キャッシュ用のディレクトリ
+css_dir         = 'src/App/BlogBundle/Resources/public/css'    # CSSを書き出すディレクトリ
+sass_dir        = 'src/App/BlogBundle/Resources/public/scss'   # SASSファイルがあるディレクトリ
+images_dir      = 'src/App/BlogBundle/Resources/public/images' # 画像用のディレクトリ
+javascripts_dir = 'src/App/BlogBundle/Resources/public/js'     # JavaScriptファイル用のディレクトリ
+cache_dir       = '../.sass-cache'                             # キャッシュ用のディレクトリ
 
 # アウトプットスタイルの選択（利用するスタイルのコメントを外して利用する）（デフォルト：expanded）
-# output_style = :expanded   #通常のアウトプットスタイル
-# output_style = :nested     #Sassなどのネストを継承したスタイル
-# output_style = :compact    #1つのCSSCSSレイクタの設定が１行になるスタイル
-# output_style = :compressed #コメントを取り除いて完全に圧縮するスタイル
+# output_style = :expanded   # 通常のアウトプットスタイル
+# output_style = :nested     # Sassなどのネストを継承したスタイル
+# output_style = :compact    # 1つのCSSCSSレイクタの設定が１行になるスタイル
+# output_style = :compressed # コメントを取り除いて完全に圧縮するスタイル
 output_style = (environment == :production) ? :compressed : :expanded
 
 # Compassで拡張された関数で使うURLを、絶対パスか相対パスかを指定（デフォルト：false[絶対パス]）
