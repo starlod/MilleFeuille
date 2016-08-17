62729e819fe0a2ac5fa3b1a7ac1916674d0982a2
starlod <starlod.ggl@gmail.com>
2016-08-17 15:58:21 +0900

    PostCRUD生成

---
+++ symfony/src/AppBundle/Entity/AppEntity.php
@@ -0,0 +1,97 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * AppEntity
+ *
+ * @ORM\MappedSuperclass
+ * @ORM\HasLifecycleCallbacks
+ *
+ */
+abstract class AppEntity
+{
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="CREATED_AT", type="datetime", nullable=false)
+     */
+    private $createdAt = null;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="UPDATED_AT", type="datetime", nullable=false)
+     */
+    private $updatedAt = null;
+
+    /**
+     * Set createdAt
+     *
+     * @param \DateTime $createdAt
+     * @return Estimate
+     */
+    public function setCreatedAt($createdAt)
+    {
+        $this->createdAt = $createdAt;
+
+        return $this;
+    }
+
+    /**
+     * Get createdAt
+     *
+     * @return \DateTime
+     */
+    public function getCreatedAt()
+    {
+        return $this->createdAt;
+    }
+
+    /**
+     * Set updatedAt
+     *
+     * @param \DateTime $updatedAt
+     * @return Estimate
+     */
+    public function setUpdatedAt($updatedAt)
+    {
+        $this->updatedAt = $updatedAt;
+
+        return $this;
+    }
+
+    /**
+     * Get updatedAt
+     *
+     * @return \DateTime
+     */
+    public function getUpdatedAt()
+    {
+        return $this->updatedAt;
+    }
+
+
+    /**
+     * INSERT 前処理
+     * @ORM\PrePersist
+     */
+    public function setPrePersist()
+    {
+        if (empty($this->createdAt) || empty($this->updatedAt)) {
+            $this->createdAt = new \Datetime();
+            $this->updatedAt = $this->createdAt;
+        }
+    }
+
+    /**
+     * UPDATE 前処理
+     * @ORM\PreUpdate
+     */
+    public function setPreUpdate()
+    {
+        $this->updatedAt = new \Datetime();
+    }
+}
